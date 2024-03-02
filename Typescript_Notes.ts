d81484268d1523ec001b001fa8638cbf41440130
mattk <mkimucsb@gmail.com>
2024-03-01 18:13:16 -0800

    beginning notes

---
+++ Typescript_Notes.ts
@@ -0,0 +1,3 @@
+// super set of javascript, allows us to provide extra features on top of javascript, developed by microsoft
+// validates your code is working correctly, writing types is optional
+// typescript is also a compiler "complies it into javascript"    
