7e4a42cc506e7113e9ce6e073a708fd0f24f08a5
mattk <mkimucsb@gmail.com>
2024-03-01 18:57:45 -0800

    types

--- Typescript_Notes.ts
+++ Typescript_Notes.ts
@@ -1,3 +1,17 @@
 // super set of javascript, allows us to provide extra features on top of javascript, developed by microsoft
 // validates your code is working correctly, writing types is optional
 // typescript is also a compiler "complies it into javascript"    
+
+
+//Types in javascript
+
+function addNumbers ( a,b) {
+    return a + b
+}
+
+console.log(addNumbers("5","6"))
+
+//gives 56 instead of 11 like we would rather have
+// typescript tells us that we should only put integers instead of a string, instead of having to type out pseudocode
+
+
