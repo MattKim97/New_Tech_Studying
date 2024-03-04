6492ca80eea641d3c0f0cb66262bd820a053b779
mattk <mkimucsb@gmail.com>
2024-03-03 20:10:59 -0800

    working with types

--- Typescript_Notes.ts
+++ Typescript_Notes.ts
@@ -72,3 +72,35 @@ console.log(example)
 // }
 
 // how to make sure to only accept numbers in ur function
+
+
+/* const reviews = [
+   {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+*/
