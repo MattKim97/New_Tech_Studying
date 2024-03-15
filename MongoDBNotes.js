3bfc7910a47f87aa7eece0b4f943495b6756c27f
mattk <mkimucsb@gmail.com>
2024-03-14 22:33:41 -0700

    intro to Mongo DB atlas

---
+++ MongoDBNotes.js
@@ -0,0 +1,7 @@
+// Introduction to Mongo Db Atlas
+
+// Atlas is a developer data platform
+// handles all the lifecycle behinds the scene
+// Replica set, stored on more than one server ensures data redundance and high availability
+
+// cluster is a group of servers that store your data
