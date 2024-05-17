9c0aef6695a0cec4a37b96ad0b2fb663f1f52dd2
mattk <mkimucsb@gmail.com>
2024-05-17 11:42:24 -0700

    java notes

---
+++ Java-study.java
@@ -0,0 +1,19 @@
+/*we will use a IDE to compile our stuff
+
+What is the JRE?
+the runtime environment has access to all of the compiled code of Java 
+if our application is running somewhere else, like another machine, that machine needs to at least have a runtime environment on it to run the compiled code.
+
+IDEIntegrated development environment
+sort of where the JDK and the JRE meets
+brings everything together
+can write in text, but why would you. need to write and compile and run it in 3 different steps
+rather than IDE doing it all for us a one step shop aas well as syntax highlighting
+can format our code for us as well, indentation, more reader spell check etc
+can do shorthands in IDE as well
+
+Intellisense
+when writing code, our editor, will try to guess what we are going to do next, will give us a list of those things
+such as variables, etc etc.
+can create an instance of a class and then hover over the variable, we will get the documentation of what that instance is
+ */
